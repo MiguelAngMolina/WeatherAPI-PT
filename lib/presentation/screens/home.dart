9bb21af198cfa6b13e3e34a804a50edb7bf2a556
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-11-16 23:48:20 -0500

    feat: agregando datepicker para hacer la consulta de los eventos

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -3,9 +3,31 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:weatherapi_gse/presentation/providers/event/event_provider.dart';
 
+
 class Home extends ConsumerWidget {
   const Home({super.key});
 
+Future<void> selectDateRange(BuildContext context, WidgetRef ref, String location ) async {
+  final DateTime now = DateTime.now();
+
+  final DateTimeRange? picked = await showDateRangePicker(
+    context: context,
+    firstDate: DateTime(now.year - 5),
+    lastDate: now,
+    initialEntryMode: DatePickerEntryMode.input,
+    locale: const Locale('es', 'ES'),
+  );
+
+  if (picked == null) return;
+
+  final start = picked.start;
+  final end = picked.end;
+
+  final startStr = "${start.year}-${start.month}-${start.day}";
+  final endStr = "${end.year}-${end.month}-${end.day}";
+
+  ref.read(eventStateProvider.notifier).loadEvents(location, startStr, endStr);
+}
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final reportAsync = ref.watch(eventStateProvider);
@@ -28,19 +50,18 @@ class Home extends ConsumerWidget {
                 ),
               ),
             ),
+
             const SizedBox(height: 12),
             ElevatedButton(
-              onPressed: () {
-                ref.read(eventStateProvider.notifier).loadEvents('38.96%2C-96.02', '2020-7-10', '2020-7-12');
-              },
-              child: const Text('Solicitar nuevo reporte'),
+              onPressed: () => selectDateRange(context, ref, "38.96%2C-96.02"),
+              child: const Text("Seleccionar rango y cargar eventos"),
             ),
 
+            const SizedBox(height: 12),
+
             ElevatedButton(
-              onPressed: () {
-                context.push('/event');
-              },
-              child: const Text('Nueva'),
+              onPressed: () => context.push('/event'),
+              child: const Text('Ir a event'),
             ),
           ],
         ),
